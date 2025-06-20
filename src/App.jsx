a46ce6b82103ccbbcf3632959a6b68b9e680e365
ehj <ehj9118@gmail.com>
2025-06-20 14:57:47 +0900

    feat: 초기 파일 생성

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <p>dmaaa</p>
+      </div>
+      <h1>Vite + React</h1>
+        <p>
+          Edit and save to test HMR
+        </p>
+    </>
+  )
+}
+
+export default App 
