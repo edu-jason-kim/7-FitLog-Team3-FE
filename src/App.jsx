9135ab7ab6447c4f541cdd7039c17592940419d7
Lee Yujin <yuwls1102@gmail.com>
2025-06-24 10:25:16 +0900

    chore: base, reset CSS 추가 및 폰트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import JournalsPost from './pages/journalsPost';
-import './css/style.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JournalsPost from "./pages/journalsPost";
+import "./css/style.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/journals/post" element={<JournalsPost />} />
-        
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App ;
+export default App;
