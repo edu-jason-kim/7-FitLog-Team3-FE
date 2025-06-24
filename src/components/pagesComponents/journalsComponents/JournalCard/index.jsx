507c40996282bb1a1e731da8822ecb1b2318253b
Lee Yujin <yuwls1102@gmail.com>
2025-06-24 18:29:27 +0900

    feat: JournalCard 컴포넌트 초안

---
+++ src/components/pagesComponents/journalsComponents/JournalCard/index.jsx
@@ -0,0 +1,12 @@
+export const JournalCard = () => {
+  return (
+    <section>
+      <div>
+        <p></p>
+      </div>
+      <h2></h2>
+      <h3></h3>
+      <p></p>
+    </section>
+  );
+};
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import { MakeButton } from "../components/pagesComponents/journalsComponents/MakeButton";
+
+export const MainPage = () => {
+  return (
+    <MakeButton
+      children="운동일지 생성하기"
+      size="medium"
+      color="brand"
+    ></MakeButton>
+  );
+};
