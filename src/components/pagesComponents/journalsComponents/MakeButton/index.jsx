f0c1cb41b232ea39edc3483e10cdbe69af8eca9e
Lee Yujin <yuwls1102@gmail.com>
2025-06-24 18:28:12 +0900

    feat: 버튼 컴포넌트 초안

---
+++ src/components/pagesComponents/journalsComponents/MakeButton/index.jsx
@@ -0,0 +1,18 @@
+import styles from "./MakeButton.module.css";
+
+/**
+ * @param {object} props - 컴포넌트 props
+ * @param {React.ReactNode} props.children - 버튼 내부에 표시될 내용
+ * @param {'large' | 'medium' | 'small' | 'xsmall'} [props.size='medium'] - 버튼 크기
+ * @param {'brand' | 'white' } [props.color='primary100'] - 버튼 색상
+ */
+
+export const MakeButton = ({ children, size = "medium", color = "brand" }) => {
+  const sizeClass = styles[size];
+  const colorClass = styles[color];
+  return (
+    <button className={`${styles.btn} ${sizeClass} ${colorClass}`}>
+      {children}
+    </button>
+  );
+};
