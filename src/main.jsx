dbefa3894309f57a8c14c8fa6138d32a1d813127
Lee Yujin <yuwls1102@gmail.com>
2025-06-24 18:28:12 +0900

    feat: 버튼 컴포넌트 초안

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "../src/css/base.css";
+import "../src/css/reset.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
