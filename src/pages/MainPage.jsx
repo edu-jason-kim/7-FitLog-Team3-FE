fcfd3a8f200c1d8a222a64096cc8ec830c9e32db
Lee Yujin <yuwls1102@gmail.com>
2025-06-25 16:59:42 +0900

    feat: EmojiValueDisplay 컴포넌트 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,3 @@
-import { MakeButton } from "../components/pagesComponents/journalsComponents/MakeButton";
-
 export const MainPage = () => {
-  return (
-    <MakeButton
-      children="운동일지 생성하기"
-      size="medium"
-      color="brand"
-    ></MakeButton>
-  );
+  return <></>;
 };
