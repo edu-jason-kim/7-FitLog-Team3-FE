946e76e8603f86676a70c7bb2ac4834f84c4e588
Lee Yujin <yuwls1102@gmail.com>
2025-06-27 11:45:29 +0900

    feat: Header 컴포넌트 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
 import styels from "./Mainpage.module.css";
-import { JournalSearchFilter } from "../components/pagesComponents/journalsComponents/JournalSearchFilter";
+import { Header } from "../components/commonComponents/Header/index.jsx";
 import { JournalExplorerSection } from "../components/pagesComponents/journalsComponents/JournalExplorerSection";
 export const MainPage = () => {
   return (
-    <div className={styels.container}>
-      <JournalExplorerSection />
-    </div>
+    <>
+      <Header></Header>
+      <div className={styels.container}>
+        <JournalExplorerSection />
+      </div>
+    </>
   );
 };
