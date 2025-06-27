a4567dbcafda3b6ca73fc2cde63eae6d0d7a4772
OhHaesang <nan1004388@gmail.com>
2025-06-27 09:34:27 +0900

    fix: 충돌해결1

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../src/css/base.css";
 import "../src/css/reset.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
