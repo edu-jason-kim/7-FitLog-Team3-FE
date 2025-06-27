751ef68f584d18cde2f84cca931bea0ff5ec157e
Lee Yujin <yuwls1102@gmail.com>
2025-06-27 11:45:29 +0900

    feat: Header 컴포넌트 구현

--- src/components/commonComponents/Header/index.jsx
+++ src/components/commonComponents/Header/index.jsx
@@ -1,13 +1,19 @@
 import styles from "./Header.module.css";
-import brandImg from "../../assets/images/brandLogo/LogoBlue.png";
-export const JournalEmojiList = () => {
+import brandImg from "../../../assets/images/brandLogo/LogoBlue.png";
+import { MakeButton } from "../MakeButton";
+/**
+ * 중요: 이 헤더는 CSS에 `position: fixed;`가 적용되어 문서 흐름에서 벗어나므로,
+ * 이 헤더 바로 아래에 오는 페이지의 주요 콘텐츠는 헤더의 높이만큼
+ * `padding-top` 또는 `margin-top` 94px을 주어야 헤더에 가려지지 않습니다.
+ */
+export const Header = () => {
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.brandLogoContainer}>
         <img src={brandImg} className={styles.brandImg} />
-        <h1>Fit Log</h1>
+        <h1 className={styles.logo}>Fit Log</h1>
       </div>
-      <MakeButton></MakeButton>
+      <MakeButton>운동일지 만들기</MakeButton>
     </header>
   );
 };
