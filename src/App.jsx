ac1d056a9f7de356711783a22815785db0ce7262
Lee Yujin <yuwls1102@gmail.com>
2025-06-27 09:20:20 +0900

    fix: rebase merge conflict 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import JournalsPost from "./pages/journalsPost";
 import "./css/style.css";
-=======
 import { MainPage } from "./pages/MainPage";
 import { PATH } from "../utils/path.js";
->>>>>>> 8fbac44 (feat: 버튼 컴포넌트 초안)
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route path="/journals/post" element={<JournalsPost />} />
-=======
         <Route path={PATH.index()} element={<MainPage />} />
->>>>>>> 8fbac44 (feat: 버튼 컴포넌트 초안)
       </Routes>
     </BrowserRouter>
   );
