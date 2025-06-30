061b3950097116b8a719ebf709a8bcfd7155cd50
edu-jason-kim <edu.jason.kim@gmail.com>
2025-06-30 18:04:23 +0900

    test

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import styles from "./Mainpage.module.css";
+import styles from "./MainPage.module.css";
 import { Header } from "../../components/commonComponents/Header/index.jsx";
 import { JournalExplorerSection } from "../../components/pagesComponents/journalsComponents/JournalExplorerSection/index.jsx";
 import { RecentJournalsSection } from "../../components/pagesComponents/journalsComponents/RecentJournalsSection/index.jsx";
